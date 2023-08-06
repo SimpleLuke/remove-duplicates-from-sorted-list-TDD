a70375fe16dd2a87d3a06ddad042fcf87c27cd80
Luke Lai <lukelai.dev@gmail.com>
2023-08-06 11:53:59 +0100

    Add and pass a test which removes deplicated elements and returns the linked list

--- deleteDuplicates.js
+++ deleteDuplicates.js
@@ -1,4 +1,12 @@
 const deleteDuplicates = (linkedList) => {
+  let current = linkedList;
+  let last = linkedList.next;
+  while (last) {
+    if (current.val == last.val) {
+      current.next = last.next;
+    }
+    last = last.next;
+  }
   return linkedList;
 };
 
--- deleteDuplicates.test.js
+++ deleteDuplicates.test.js
@@ -7,4 +7,20 @@ describe("deleteDuplicates", () => {
     const linkedListTwo = { val: 1, next: { val: 2, next: null } };
     expect(deleteDuplicates(linkedListTwo)).toEqual(linkedListTwo);
   });
+
+  it("removes duplicated elements and returns the linked list", () => {
+    const linkedListOne = {
+      val: 1,
+      next: { val: 1, next: null },
+    };
+    const outputOne = { val: 1, next: null };
+    expect(deleteDuplicates(linkedListOne)).toEqual(outputOne);
+
+    const linkedListTwo = {
+      val: 1,
+      next: { val: 1, next: { val: 2, next: null } },
+    };
+    const outputTwo = { val: 1, next: { val: 2, next: null } };
+    expect(deleteDuplicates(linkedListTwo)).toEqual(outputTwo);
+  });
 });
